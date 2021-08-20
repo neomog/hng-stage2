6451213c1a37617f52ff295db55a88634bb87f80
Ohiare Nathaniel <ohiarenathanielmog@gmail.com>
2021-08-20 09:00:25 +0100

    added index and composer file

---
+++ resume/index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+        <!-- header start -->
+        <div id="header" class="section">
+            <img alt="" class="img-circle" src="hng.jpg">
+            <p>Ohiare Nathaniel</p>
+        </div>
+        <!-- header end -->
+        
+        <!-- About Me section start -->
+        <div class="section">
+            <h1><span>About Me</span></h1>
+            <p>
+                Hello! I'm <strong>Nath</strong>. I am a software developer. I don't just code only because i am passionate about it, but also because it helps me reason logically. Coding has also improved my technical reasoning. I aim to achieve a professional and personal fulfillment in my duties. To achieve this, I will discharge my potential, natural affinity, diligence and knowledge, working with untiring effort and dedication to providing service for the benefit of others.</p>
+        </div>
+        <!-- About Me section end -->
+        
+  
+              <!-- Experience section start -->
+              <div class="section">
+            <h1><span>Experience</span></h1>
+            <ul>
+                <li>Fireswitch Technology -- Software Developer (2021)<br />
+                </li>
+                <li>Akada Educational Resources -- System Analyst (2020) <br />
+
+            </ul>
+        </div>
+        <!-- Experience section end -->
+        
+        
+        <!-- My Skills section start -->
+        <div class="section">
+            <h1><span>My Skills</span></h1>
+            <ul>
+                <li>HTML <br />
+                    <progress min="0" max="100" value="80"></progress>
+                </li>
+                <li>JavaScript <br />
+                    <progress min="0" max="100" value="70"></progress>
+                </li>
+                <li>PHP <br />
+                    <progress min="0" max="100" value="90"></progress>
+                </li>
+                <li>SQL <br />
+                    <progress min="0" max="100" value="50"></progress>
+                </li>
+                <li>Python <br />
+                    <progress min="0" max="100" value="30"></progress>
+                </li>
+            </ul>
+        </div>
+        <!-- My Skills section end -->
+
+        <!-- Form section start -->
+       <div class="section">
+            <h1><span>Contact Me</span></h1>
+
+                 
+            <form method="post" action="handler.php">
+                <input name="name" placeholder="Name" type="text" required /><br/>
+                <input name="email" placeholder="Email" type="email" required /><br/>
+                <textarea name="message" placeholder="Message" required ></textarea>
+                <input type="submit" value="SEND" class="submit" />
+            </form>
+        </div>
+        <!-- Form section end -->
+        
+        <!-- Contacts section start -->
+        <div style="" id="contacts">
+            <h1><span>Credit</span></h1>
+            <div>
+                <a href="https://ingressive.org/" target="_blank">
+                    <img alt="SoloLearn" src="ifg.png" />
+                </a>
+                <a href="https://internship.zuri.team/">
+                    <img alt="Facebook" src="zuri.png"/>
+                </a>
+                <a href="https://hng.tech/">
+                    <img alt="Twitter" src="hngg.png" />
+                </a>
+            </div>
+        </div>
+        <!-- Contacts section end -->
+
+        
+    </body>
+</html>
