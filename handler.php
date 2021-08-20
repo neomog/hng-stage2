8ee12e437cde8778b5c7e47f92cfe573d1978a7b
Nathaniel Ohiare <36834264+neomog@users.noreply.github.com>
2021-08-20 13:30:24 +0100

    Update handler.php

--- handler.php
+++ handler.php
@@ -3,8 +3,7 @@
 $request_method = $_SERVER['REQUEST_METHOD'];
 
 if ($request_method == "POST") {
-    echo 'hello';
-    echo $_POST['name'];
+    header("location: message.php");
 }
 
 
@@ -47,24 +46,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             $messageErr = "Please enter a valid comment.";
         }
     }
-        // Check input errors before sending email
-        if(empty($nameErr) && empty($emailErr) && empty($messageErr)){
-            // Recipient email address
-            $to = 'webmaster@example.com';
-            
-            // Create email headers
-            $headers = 'From: '. $email . "\r\n" .
-            'Reply-To: '. $email . "\r\n" .
-            'X-Mailer: PHP/' . phpversion();
-            
-            // Sending email
-            if(mail($to, $subject, $message, $headers)){
-                echo '<p class="success">Your message has been sent successfully!</p>';
-            } else{
-                echo '<p class="error">Unable to send email. Please try again!</p>';
-            }
-        }
-    }
+
 
     function filterName($field){
         // Sanitize user name
