822bd5299bcda7654199289b44fe81e47bed2910
Nathaniel Ohiare <36834264+neomog@users.noreply.github.com>
2021-08-20 13:25:04 +0100

    Update handler.php

--- handler.php
+++ handler.php
@@ -50,19 +50,19 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         // Check input errors before sending email
         if(empty($nameErr) && empty($emailErr) && empty($messageErr)){
             // Recipient email address
-            $to = 'webmaster@example.com';
+//             $to = 'webmaster@example.com';
             
-            // Create email headers
-            $headers = 'From: '. $email . "\r\n" .
-            'Reply-To: '. $email . "\r\n" .
-            'X-Mailer: PHP/' . phpversion();
+//             // Create email headers
+//             $headers = 'From: '. $email . "\r\n" .
+//             'Reply-To: '. $email . "\r\n" .
+//             'X-Mailer: PHP/' . phpversion();
             
             // Sending email
-            if(mail($to, $subject, $message, $headers)){
-                echo '<p class="success">Your message has been sent successfully!</p>';
-            } else{
-                echo '<p class="error">Unable to send email. Please try again!</p>';
-            }
+//             if(mail($to, $subject, $message, $headers)){
+//                 echo '<p class="success">Your message has been sent successfully!</p>';
+//             } else{
+//                 echo '<p class="error">Unable to send email. Please try again!</p>';
+//             }
         }
     }
 
